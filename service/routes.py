cc65e1b04ff20e5874a22e3f05ebe92d6dd274ca
jondhill333 <jondhill1984@gmail.com>
2024-10-01 14:59:03 +0100

    fix linting issue with build

--- service/routes.py
+++ service/routes.py
@@ -61,13 +61,6 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 
-# ... place you code here to LIST accounts ...
-
-@app.route("/accounts", methods=["GET"])
-    ######################################################################
-    # LIST ALL ACCOUNTS
-    ######################################################################
-
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
